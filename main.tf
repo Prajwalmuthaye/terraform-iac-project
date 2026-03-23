d84f903611e039a855ac5c6aa2a887a95b0faaf4
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 14:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ provider "aws"{
 resource "aws_instance" "web04" {
   ami           = "ami-0f559c3642608c138"
   instance_type = "t3.micro"
-
-  vpc_security_group_ids = ["vpc-0e92edc754644d44a"]
   subnet_id = "subnet-04a25832138e1fa3e"
   
   tags = {
