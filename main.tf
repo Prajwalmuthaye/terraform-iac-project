be8081d4950607e0546107c45a9bfc8480b8ce76
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 12:48:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 provider "aws"{
 	region = "ap-south-1"
 }
-resource "aws_instance" "example" {
-  ami                     = "ami-0f559c3642608c138"
-  instance_type           = "t2.micro"
- }
+resource "aws_instance" "app_server" {
+  ami           = "ami-0abcdef12345"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer"
+  }
+}
