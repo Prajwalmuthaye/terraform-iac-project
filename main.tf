763a17305e2fff6b56dbb59c63c297001634e39c
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 13:02:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
 	region = "ap-south-1"
 }
 resource "aws_instance" "app_server" {
-  ami           = "ami-0abcdef12345"
+  ami           = "ami-0f559c3642608c138"
   instance_type = "t2.micro"
 
   tags = {
