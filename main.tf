b6ab3780a28746482c241272880f901781a8f01c
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 14:04:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,13 @@
 provider "aws"{
 	region = "ap-south-1"
 }
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-resource "aws_instance" "myweb01" {
-  ami           = data.aws_ami.amazon_linux.id
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f559c3642608c138"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [ "main" ]
-  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [ "vpc-0e92edc754644d44a" ]
+  subnet_id = "subnet-04a25832138e1fa3e"
+  security_groups = "sg-0959c8a7da48061aa"
   tags = {
     Name = "AppServer"
   }
