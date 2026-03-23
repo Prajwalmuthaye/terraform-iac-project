4889144ad5f0cb2d358e8c048057b1f2689870f8
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 12:45:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,5 +4,4 @@ provider "aws"{
 resource "aws_instance" "example" {
   ami                     = "ami-0f559c3642608c138"
   instance_type           = "t2.micro"
-  subnet_id               = "subnet-0c5716b028d950bcd"
-}
+ }
