c96e58101c80cdb143b1717fdf62fb1365c722d0
Prajwalmuthaye <108486802+Prajwalmuthaye@users.noreply.github.com>
2026-03-23 13:43:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 provider "aws"{
 	region = "ap-south-1"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
 data "aws_ami" "amazon_linux" {
   most_recent = true
 
@@ -14,7 +26,16 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "myweb01" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [ "main" ]
+  subnet_id = var.subnet_id["public"]
+  tags = {
+    Name = "AppServer"
+  }
+}
 
+
+  
+  
   tags = {
     Name = "AppServer"
   }
